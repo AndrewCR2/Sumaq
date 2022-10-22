8dde757a358f7c866f62b5070492a9eb8fbd7b35
AndrewCR2 <81635559+AndrewCR2@users.noreply.github.com>
2022-10-22 10:36:55 -0500

    primer comit

--- src/main/java/com/certus/spring/controller/HomeController.java
+++ src/main/java/com/certus/spring/controller/HomeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.certus.spring.models.Personaje;
 
 @Controller
-@RequestMapping("/app")
+@RequestMapping("/")
 public class HomeController {
 	
 	@Value("${title.generic}")
@@ -21,24 +21,31 @@ public class HomeController {
 	@GetMapping({"/home","inicio","/","Home","Inicio"})
 	public String HolaMundo(Model model) {	
 		
-		Personaje personaje1  =new  Personaje();
+		//Personaje personaje1  =new  Personaje();
 		
-		personaje1.setNombre("Luffy");
+		/*personaje1.setNombre("Luffy");
 		personaje1.setAlias(",Luffy Alias");
 		personaje1.setTipoFruta("Luffy Tipo Fruta");
 		personaje1.setHabilidad("Luffy Habilidad");	
 		personaje1.setTripulacion("Luffy Tripulacion");
-		personaje1.setRecompensa("13245");
+		personaje1.setRecompensa("13245");*/
 		
 		
 		
-		List<Personaje> lista = new ArrayList<>();
-		lista.add(personaje1);
-	
+	/*	List<Personaje> lista = new ArrayList<>();
+		lista.add(personaje1);*/
+	     
+		List<String> menu = new ArrayList<>();
 		
-		model.addAttribute("TituloPagina",titlePage);
-		model.addAttribute("titulo","Seccion J98");
-		model.addAttribute("lista",lista);
+		 menu.add("Nosotros");
+		 menu.add("Ofecemos");
+		 menu.add("Productos");
+		 menu.add("Contactenos");
+		 menu.add("Pedir Aqui");
+		
+	   model.addAttribute("TituloPagina",titlePage);
+		//model.addAttribute("titulo","Seccion J98");
+		model.addAttribute("menu",menu);
 		
 		
 		return "index";
