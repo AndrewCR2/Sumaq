0ede6db92ed8ad719ba50c2e0d176d44f6df3aa2
g-susvs <jesus.savs19@gmail.com>
2022-11-14 21:53:09 -0500

    diseño base del form de categorias

--- src/main/java/com/certus/spring/controller/CategoriaController.java
+++ src/main/java/com/certus/spring/controller/CategoriaController.java
@@ -4,21 +4,28 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/categoria")
 public class CategoriaController {
 	
 	@Value("${title.generic}")
 	private String titlePage;
 	
-	@GetMapping({"/categoria","/CATEGORIA"})
-	public String ListaCategoria(Model model) {	
+	@GetMapping({"/","lista","LISTA"})
+	public String listaCategoria(Model model) {	
 	     
 	   model.addAttribute("TituloPagina",titlePage);
 	 
-		return "categoria_lista";
+		return "Categoria/categoria_lista";
+	}
+	
+	@GetMapping("/form")
+	public String formCategoria(Model model) {
+		
+		return "Categoria/categoria_form";
 	}
 
 }
---
+++ src/main/java/com/certus/spring/models/Categoria.java
@@ -0,0 +1,20 @@
+package com.certus.spring.models;
+
+public class Categoria {
+	private String nombre;
+	private Boolean estado;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Boolean getEstado() {
+		return estado;
+	}
+	public void setEstado(Boolean estado) {
+		this.estado = estado;
+	}
+    
+}
