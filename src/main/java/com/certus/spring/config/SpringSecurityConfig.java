fd83cfcb92a5974f254441568da5d0fe12a2413a
JhanCarlos18 <81634591+JhanCarlos18@users.noreply.github.com>
2022-12-12 22:50:50 -0500

    Spring Security

--- src/main/java/com/certus/spring/config/SpringSecurityConfig.java
+++ src/main/java/com/certus/spring/config/SpringSecurityConfig.java
@@ -2,10 +2,12 @@ package com.certus.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -22,8 +24,30 @@ public class SpringSecurityConfig {
 				.password("12345")
 				.roles("SuperAdmin")
 				.build();
-		return new InMemoryUserDetailsManager(UsuarioOne);
+		
+		UserDetails Usuario = usuarios
+				.username("Invitado")
+				.password("12345")
+				.roles("User")
+				.build();
+		return new InMemoryUserDetailsManager(UsuarioOne, Usuario);
 	}
-	
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests(
+				(req) -> req
+						.antMatchers("/home","/bootstrap/**","/CSS/**","/img/**","/JS/**").permitAll()
+						.antMatchers("/usuario/listar").hasRole("User")
+						.antMatchers("/usuario/Editar/**").hasRole("SuperAdmin")
+						.antMatchers("/categoria/listar").hasRole("User")
+						.antMatchers("/categoria/Editar/**").hasRole("SuperAdmin")
+						.antMatchers("/home/Productos").hasRole("User")
+						.antMatchers("/home/Editar/**").hasRole("SuperAdmin")
+						.anyRequest().authenticated()
+				).formLogin();
+		
+		return http.build();
+	}
 
 }
